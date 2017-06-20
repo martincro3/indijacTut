a4a764a3db3605f007c3a7aaaae5f705a0bd4781
Martin Markovic <martin.markovic@gauss.hr>
2017-06-20 14:57:28 +0200

    commitanje

---
+++ back/api/controllers/PostController.js
@@ -0,0 +1,61 @@
+/**
+ * PostController
+ *
+ * @description :: Server-side logic for managing Posts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PostController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.findOne()`
+   */
+  findOne: function (req, res) {
+    return res.json({
+      todo: 'findOne() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.findAll()`
+   */
+  findAll: function (req, res) {
+    return res.json({
+      todo: 'findAll() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `PostController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  }
+};
+
